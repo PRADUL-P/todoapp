08ef70b9f5044992ecc408f4f3549fd47c4f088b
Pradul p <pradul.p123@gmail.com>
2021-09-12 10:20:02 +0530

    + todo

---
+++ list/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from list . views import home, update, delete
+
+urlpatterns = [
+    path('', home,name='home'),
+    path('update/<int:todo_id>/', update,name='update'),
+    path('delete/<int:todo_id>/',delete,name='delete'),
+]
